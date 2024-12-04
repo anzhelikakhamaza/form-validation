c14616ac0263b1f04a53c804705717f7297ef92f
Aznhelika Khamaza <anzhelikakhamaza@gmail.com>
2024-12-04 10:20:14 -0600

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,23 @@
 import FormSubmit from "@/components/FormSubmit.vue";
 import FormDisplay from "@/components/FormDisplay.vue";
 
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const userData = reactive({
-  name: '',
-  email: '',
-  password: 'user password',
-  passwordConfirmation: 'user password confirmation',
+  name: "",
+  email: "",
+  password: "",
+  passwordConfirmation: "",
 });
 
 const handleFormSubmit = (userData) => {
-  console.log('Реєстрація успішна. Ласкаво просимо, ', userData.name);
-}
-
+  console.log("Реєстрація успішна. Ласкаво просимо, ", userData.name);
+};
 </script>
 
 <template>
-  <FormSubmit @submit="handleFormSubmit" user="userData"/>
-  <FormDisplay :user="userData"/>
+  <FormSubmit @submit="handleFormSubmit" user="userData" />
+  <FormDisplay :user="userData" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
